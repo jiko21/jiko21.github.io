f5d30349bdf16b6ccb30ca78373b589222ec0692
jiko21 <daikikojima0531@icloud.com>
2022-05-07 00:57:57 +0900

    update: contents

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -3,11 +3,18 @@ import profile from './profile';
 import skills from './skills';
 import slides from './slides';
 import works from './works';
+import uraKeireki from './ura-keireki';
+import uraSkill from "./ura-skill";
 
 export const items = {
-	about,
-	profile,
-	skills,
-	slides,
-	works,
+  about,
+  profile,
+  skills,
+  slides,
+  works,
+}
+
+export const uraItems = {
+  uraKeireki,
+  uraSkill,
 }
---
+++ src/contents/ura-skill.ts
@@ -0,0 +1,18 @@
+export default [
+  {
+    lang: 'プログラミング',
+    skill: 100,
+  },
+  {
+    lang: '料理',
+    skill: 100,
+  },
+  {
+    lang: 'ギター',
+    skill: 100,
+  },
+  {
+    lang: '勉強',
+    skill: 100,
+  }
+];
