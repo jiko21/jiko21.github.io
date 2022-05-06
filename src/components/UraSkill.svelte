9b65ed1aebfd818301624dfac0b31b5ff9899fc3
jiko21 <daikikojima0531@icloud.com>
2022-05-07 00:58:25 +0900

    update: contents

---
+++ src/components/UraSkill.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+import UraBar from "./UraBar.svelte";
+export let lang="";
+export let skill=0;
+</script>
+<div class="container">
+    <div class="lang">{lang}</div>
+    <UraBar skill={skill} />
+    <div class="skill">{skill}%</div>
+</div>
+<style lang="scss">
+    .container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 4px;
+    }
+    .lang {
+      display: flex;
+      justify-content: flex-end;
+      padding-right: 8px;
+      width: 140px;
+    }
+    .skill {
+      display: flex;
+      justify-content: flex-end;
+      padding-left: 8px;
+      width: 60px;
+    }
+</style>
---
+++ src/components/UraTable.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export type TableItem = {
+    [key: string]: any;
+  };
+  export let items: TableItem[] = []
+  export let columns: string[] = [];
+  export let tableKey: string[] = [];
+</script>
+<table>
+    <thead>
+        <tr>
+            {#each columns as column, i (i)}
+                <th>{column}</th>
+            {/each}
+        </tr>
+    </thead>
+    <tbody>
+        {#each items as item, i (i)}
+            <tr>
+                {#each tableKey as key, j (j)}
+                    <td>{item[key]}</td>
+                {/each}
+            </tr>
+        {/each}
+    </tbody>
+</table>
+<style lang="scss">
+table {
+  border: double 4px #000000;
+}cおんそぇ
+thead, tr, th, td {
+  border: solid 1px #000000
+}
+</style>
