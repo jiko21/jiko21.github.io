fdb718bc7d6f12e6b86eff2633059f16bc132c67
jiko21 <daikikojima0531@icloud.com>
2023-06-24 16:09:23 +0900

    fix: contents

--- src/contents/works.ts
+++ src/contents/works.ts
@@ -1,41 +1,34 @@
 export default {
   contents: [
     {
-      title: 'Boox!',
-      link: 'https://play.google.com/store/apps/details?id=app.kojima.jiko.boox',
-      content: '気になるあの人が読んだ本がわかる! 新世代書籍SNS!',
-      img: 'boox-header.png',
-    },
-    {
-      title: 'Mark Park',
-      link: 'https://play.google.com/store/apps/details?id=com.jiko.bycicle',
-      content: `止めた自転車の位置を記録できる!
-京都府民必見の自転車ライフ補助アプリ!`,
-      img: 'markpark.webp'
-    },
-    {
-      title: 'CharTo for Web',
-      link: 'https://charto-abc1d.firebaseapp.com/',
-      content: `1日で完結するタスク管理アプリCharToがWebに!
-ソーシャル連携でどこでもタスク管理!!`,
-      img: 'CharTo-web.webp ',
-    },
-    {
-      title: 'LibCom',
-      link: 'https://apps.apple.com/jp/app/libcom/id1455763687',
-      content: `積読を管理できる読書管理アプリLibCom!
-これでもあなたも積読解消!`,
-      img: 'libcom.png',
-    },
-    {
       title: 'Flav MD',
       link: 'https://jiko21.me/flav-md/',
       content: 'markdown parser with css style',
     },
     {
+      title: 'Flav MD',
+      link: 'https://github.com/flav-md-engine/',
+      content: 'rust implementation of flav-md',
+    },
+    {
       title: 'GOMI',
       link: 'https://jiko21.me/Gomi',
       content: 'Branch delete tool made by Golang',
-    }
+    },
+    {
+      title: 'Comifer',
+      link: 'https://jiko21.me/Comifer',
+      content: 'Commit format tools',
+    },
+    {
+      title: 'shellio',
+      link: 'https://jiko21.me/shellio/',
+      content: 'snapshot testing tools for cli',
+    },
+    {
+      title: 'color-picker',
+      link: 'https://color-picker-jiko21.vercel.app/',
+      content: 'color picker app (generating OG with colormaps)',
+    },
   ]
 }
